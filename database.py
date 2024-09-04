def925251c313052614f7031eedeef3ea8994226
EdnanB <ednanbesic96@gmail.com>
2024-09-04 17:31:18 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,16 @@
-# Import neophodnih modula
 import mysql.connector
 from mysql.connector import errorcode
 
-# Naziv baze podataka
 DB_NAME = 'user_odjeca'
 
-# Konfiguracija za konekciju na MySQL server
 config = {
-    'user': 'root',
-    'password': 'ednan2544',
+    'user': 'user',
+    'password': 'pw',
     'host': '127.0.0.1',
     'port': 3306
 }
 
-# Funkcija za kreiranje baze podataka
+
 def create_database(cursor):
     try:
         cursor.execute(
@@ -23,15 +20,15 @@ def create_database(cursor):
         print(f"Failed creating database: {err}")
         exit(1)
 
-# Konekcija na MySQL server
+
 cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor()
 
 try:
-    # Pokušaj korištenja baze podataka
+
     cursor.execute(f"USE {DB_NAME}")
 except mysql.connector.Error as err:
-    # Ako baza podataka ne postoji, kreiraj je
+
     print(f"Database {DB_NAME} does not exist.")
     if err.errno == errorcode.ER_BAD_DB_ERROR:
         create_database(cursor)
@@ -41,6 +38,6 @@ except mysql.connector.Error as err:
         print(err)
         exit(1)
 
-# Zatvaranje kursora i konekcije
+
 cursor.close()
 cnx.close()
